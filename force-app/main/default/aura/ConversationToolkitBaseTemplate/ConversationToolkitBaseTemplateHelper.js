d0f5bfc484022083fbb19fbad9456fa27c9f7a93
Neil Armstrong <nasalesforcedemo@gmail.com>
2020-10-24 19:56:58 -0500

    Adding in sample base component

---
+++ force-app/main/default/aura/ConversationToolkitBaseTemplate/ConversationToolkitBaseTemplateHelper.js
@@ -0,0 +1,42 @@
+({
+    subscribeToVoiceToolkit: function(cmp) { 
+        cmp._conversationEventListener = $A.getCallback(this.voiceConversationEventListener.bind(this, cmp));
+        cmp.find('voiceToolkitApi').addConversationEventListener('TRANSCRIPT', cmp._conversationEventListener);
+    },
+    
+    unsubscribeFromVoiceToolkit: function(cmp) {
+        cmp.find('voiceToolkitApi').removeConversationEventListener('TRANSCRIPT', cmp._conversationEventListener);
+    },
+    
+    invokeNBA: function(cmp, transcriptText) {
+        var transcriptVariable = {ConversationKey: transcriptText};
+        cmp.find('voiceToolkitApi').updateNextBestActions(cmp.get('v.recordId'), transcriptVariable);
+    },
+    
+    // Voice Transcripts (Customer and Agent)
+    voiceConversationEventListener: function(cmp, transcript) {
+        var transcriptText = transcript.detail.content.text;
+        var speaker = transcript.detail.sender.role;            
+        var recordId = cmp.get("v.recordId");
+        //Confirm that the component is on a Voice Call Record page
+        if (recordId.startsWith("0LQ")){
+            this.runCustomFuntion(cmp, transcriptText, speaker);
+        }
+    },
+    // Chat/Messaging Transcripts (Customer and Agent)
+    chatConversationEventListener: function(cmp, evt, speaker) {
+        var transcriptText = evt.getParam('content');
+        var recordId = cmp.get("v.recordId");
+        var chatRecordId = evt.getParam("recordId");       
+        //Confirm that the Event came from the Chat that the component is on
+        if (recordId.includes(chatRecordId)){
+            this.runCustomFuntion(cmp, transcriptText, speaker);        
+        }
+    },
+    
+    runCustomFuntion: function(cmp, transcriptText, speaker){
+        ////////////////////////////
+        //INSERT CUSTOMER LOGIC HERE
+        ////////////////////////////
+    }
+})
